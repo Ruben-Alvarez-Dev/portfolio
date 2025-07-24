236e0bf8573f93ef541c1e06781f1d0e4d0d5782
ruben <ruben@Mini-de-ruben.home>
2025-07-24 21:13:01 +0200

    header elements responsive

--- src/components/layout/HeaderComponent/HeaderComponent.jsx
+++ src/components/layout/HeaderComponent/HeaderComponent.jsx
@@ -1,33 +1,35 @@
 import "./HeaderComponent.css";
 
-import HeaderGroup from './HeaderGroup/HeaderGroup';
-
 const HeaderComponent = () => {
-  
   return (
     <header className="HeaderComponent">
-      <HeaderGroup className="HeaderGroup logo">
+    <nav>
+      <div className="logo-group">
         <span className="HeaderLogo">Logo</span>
         <span className="HeaderTitle">Mi Portafolio</span>
-      </HeaderGroup>
-      
-      <HeaderGroup className="HeaderGroup links">
+      </div>
+
+      <div className="links-group">
         <span className="HeaderLink">Link 1</span>
         <span className="HeaderLink">Link 2</span>
         <span className="HeaderLink">Link 3</span>
         <span className="HeaderLink">Link 4</span>
-      </HeaderGroup>
-      
-      <HeaderGroup className="HeaderGroup switches">
+      </div>
+
+      {/* Spacer para móvil - se expande cuando los links están ocultos */}
+      <div className="mobile-spacer"></div>
+
+      <div className="switch-group">
         <span className="HeaderSwitch">Switch 1</span>
         <span className="HeaderSwitch">Switch 2</span>
-      </HeaderGroup>
-      <HeaderGroup className="HeaderGroup burguer">
-        <span className="HeaderBurguer">Burguer</span>
-      </HeaderGroup>      
+      </div>
       
+      <div className="burguer-group">
+        <span className="HeaderBurguer">Burguer</span>
+      </div>
+    </nav>
     </header>
   );
-};
+}
 
 export default HeaderComponent;
